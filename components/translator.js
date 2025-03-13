eefc451d3e3cbd516d09bf48f12fbcbce0c4b0d6
MobuchiElly <ugwueleazer@gmail.com>
2025-03-13 13:30:21 +0100

    feat: implementation translation logic for american to british spellings and vice versa

--- components/translator.js
+++ components/translator.js
@@ -3,8 +3,62 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
-class Translator {
+/*
+    Get the key value lookups
+    word spellings: americanOnly, americanToBritishSpelling
+                     britishOnly, reverse americanToBritishSpelling
+    titles: americanToBritishTitles
+            reverse americanToBritishTitles
+    time: regex for 
+*/
 
+class Translator {
+    constructor(text, locale){
+        this.text = text;
+        this.locale = locale;
+    }
+    translate(text, locale){ //havent inplemnted time yet
+        let sanitisedText = this.sanitiseInput(text);
+        let translation = sanitisedText;
+        if (locale == "american-to-british"){
+            let americanDict = this.combineObjs(americanOnly, americanToBritishSpelling, americanToBritishTitles);
+            for (const [key, value] of Object.entries(americanDict)){
+                let regex = new RegExp(`\\b${key}\\b`, `gi`);
+                // let regex = /\b${key}\b/gi;
+                //console.log(regex)
+                translation = translation.replace(regex, `<span class="highlight">${value}</span>`);
+            }
+        }
+        if (locale == "british-to-american"){
+            const britishToAmericanSpelling = this.reverseObj(americanToBritishSpelling);
+            const britishToAmericanTitles = this.reverseObj(americanToBritishTitles);
+            const britishDict = this.combineObjs(britishOnly, britishToAmericanSpelling, britishToAmericanTitles)
+            for (const [key, value] of Object.entries(britishDict)){
+                let regex = new RegExp(`\\b${key}\\b`, `gi`);
+                translation = translation.replace(regex, `<span class="highlight">${value}</span>`);
+            }
+        }
+        return translation == sanitisedText ? "Everything looks good to me!" : translation;
+    }
+    sanitiseInput(text){
+        return text.trim();
+    }
+    combineObjs(obj1, obj2, obj3){
+        const combinedDict = {};
+        
+        for (const [key, value] of Object.entries(obj1)) combinedDict[key] = value;
+        
+        for (const [key, value] of Object.entries(obj2)) combinedDict[key] = value;
+
+        for (const [key, value] of Object.entries(obj3)) combinedDict[key] = value;
+
+        return combinedDict;
+    }
+    reverseObj(obj){
+        let reversedObj = {};    
+        for (let [key, value] of Object.entries(obj)) reversedObj[value] = key;
+        return reversedObj;
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -3,18 +3,24 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
   const translator = new Translator();
+  
 
   app.route('/api/translate')
     .post((req, res) => {
       const {text, locale} = req.body;
+      
+
       if (text == undefined || locale == undefined) return res.json({ error: 'Required field(s) missing' })
       if(text == '') return res.json({ error: 'No text to translate' })
-      if(locale != 'american-to-british' || locale != 'british-to-american') return res.json({ error: 'Invalid value for locale field' });
+      if(locale != 'american-to-british' && locale != 'british-to-american') return res.json({ error: 'Invalid value for locale field' });
+      const translation = translator.translate(text, locale);
+      console.log(translation);
+      
+      
+
       //shpuld handle time translation as well
-      console.log(req.body);
-      //return res.send("Tracking...");
-      return res.status(201).json({ "text": "testing text", "translation": 'testing translation' });
+      let favourite = "hey"
+      return res.status(201).json({ 'text': 'testing text', translation });
     });
 };
